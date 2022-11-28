45cc2fc1782ba34e95004ca0714abdb7440a0932
qin <qzb0407@126.com>
2022-11-28 17:32:34 +0800

    17：32 test

--- demo01/src/components/Login.vue
+++ demo01/src/components/Login.vue
@@ -14,7 +14,7 @@
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="login('form')">登录</el-button>    
-                
+                    <el-button type="primary" @click="login('form')">登录</el-button>  
                 </el-form-item>
             </el-form>
         </el-card>
